6e62e6126f50fbf2aa50788b1f42733de3d93445
Henko Beukes <henkobeukes@gmail.com>
2020-03-24 14:32:17 +0200

    First commit

---
+++ wunderScraper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+
+city = input("Type in the city:\n> ")
+city = city.lower().replace(' ', '-')
+city_url = "https://www.wunderground.com/weather/za/"+city
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Firefox(options=options)
+driver.get(city_url)
+
+driver.implicitly_wait(30)
+settings_button = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/button/i')
+settings_button.click()
+# print('settings selected')
+driver.implicitly_wait(10)
+
+choose_celcius = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/div/div/a[2]').click()
+
+# print('celsius selected')
+driver.implicitly_wait(30)
+
+site = driver.page_source
+# print("grabbed page")
+
+soup = BeautifulSoup(site, 'lxml')
+city_name = soup.find('h1').span.text
+print(city_name)
+forecast_block = soup.find('div', class_='forecast-wrap')
+forecast_blocks = [forecast_block, forecast_block.next_sibling,
+           forecast_block.next_sibling.next_sibling]
+for block in forecast_blocks:
+    anchor = block.find('a', class_='hook')
+    forecast = anchor.next_sibling.next_sibling.text
+    day = block.find('span', class_='day').text
+    print(f"{day}: {forecast}")
+
+driver.quit()  # to close the browser entirely
+
