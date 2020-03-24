a81471ee305e9818cb3b1f3ad99efa4286a9541a
Henko Beukes <henkobeukes@gmail.com>
2020-03-24 17:03:06 +0200

    Added country codes

--- wunderScraper.py
+++ wunderScraper.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+import pickle
 
 city = input("Type in the city:\n> ")
 city = city.lower().replace(' ', '-')
-city_url = "https://www.wunderground.com/weather/za/"+city
+
+country = input("Type in the country: \n> ")
+
+country = country.lower()
+
+with open('country_codes.pk', 'rb') as file:
+    country_codes = pickle.load(file)
+
+code = country_codes[country]
+
+city_url = "https://www.wunderground.com/weather/"+code+"/"+city
+
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
