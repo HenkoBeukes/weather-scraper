8d8a20619964c3e4174232e9697c702985ada00f
Henko Beukes <henkobeukes@gmail.com>
2020-03-25 12:30:19 +0200

    Added exception handling Added country code handiling including USA states

--- wunderScraper.py
+++ wunderScraper.py
@@ -3,8 +3,15 @@
 from bs4 import BeautifulSoup
 import pickle
 
-city = input("Type in the city:\n> ")
-city = city.lower().replace(' ', '-')
+city = input("Type in the city:(for United States of America add the two letter state "
+             "code):\n> ")
+city_data = city.split(' ')
+if len(city_data[-1]) == 2:
+    state = city_data.pop().lower()
+    city = ('-').join(city_data).lower()
+    city = state+'/'+city
+else:
+    city = ('-').join(city_data).lower()
 
 country = input("Type in the country: \n> ")
 
@@ -12,46 +19,51 @@ country = country.lower()
 
 with open('country_codes.pk', 'rb') as file:
     country_codes = pickle.load(file)
-
-code = country_codes[country]
+try:
+    code = country_codes[country]
+    # print(code)
+except KeyError:
+    print("Check the real name of the country as per: https://www.worldatlas.com/aatlas/ctycodes.htm ")
+    quit()
 
 city_url = "https://www.wunderground.com/weather/"+code+"/"+city
-
+# print(city_url)
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.headless = True
-
 driver = webdriver.Firefox(options=options)
-driver.get(city_url)
 
-driver.implicitly_wait(30)
-settings_button = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/button/i')
-settings_button.click()
-# print('settings selected')
-driver.implicitly_wait(10)
+try:
+    driver.get(city_url)
+    driver.implicitly_wait(30)
+    settings_button = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/button/i')
+    settings_button.click()
+    driver.implicitly_wait(10)
 
-choose_celcius = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/div/div/a[2]').click()
+    choose_celcius = driver.find_element_by_xpath('/html/body/app-root/app-today/one-column-layout/wu-header/div/div/lib-settings/header/div/div/a[2]').click()
 
-# print('celsius selected')
-driver.implicitly_wait(30)
+    driver.implicitly_wait(30)
 
-site = driver.page_source
-# print("grabbed page")
+    site = driver.page_source
+    # print("grabbed page")
 
-soup = BeautifulSoup(site, 'lxml')
-city_name = soup.find('h1').span.text
-print(city_name)
-forecast_block = soup.find('div', class_='forecast-wrap')
-forecast_blocks = [forecast_block, forecast_block.next_sibling,
-           forecast_block.next_sibling.next_sibling]
-for block in forecast_blocks:
-    anchor = block.find('a', class_='hook')
-    forecast = anchor.next_sibling.next_sibling.text
-    day = block.find('span', class_='day').text
-    print(f"{day}: {forecast}")
+    soup = BeautifulSoup(site, 'lxml')
 
-driver.quit()  # to close the browser entirely
+    city_name = soup.find('h1').span.text
+    print(city_name)
+    forecast_block = soup.select("div.forecast-wrap.ng-star-inserted")   # gives a list
+
+    for block in forecast_block:
+        anchor = block.find('a', class_='hook')
+        forecast = anchor.next_sibling.next_sibling.text
+        day = block.find('span', class_='day').text
+        print(f"{day}: {forecast}")
+except AttributeError:
+    print("Something went wrong while parsing the page, one of the tags does not have "
+          "the required attribute")
+finally:
+    driver.quit()  # to close the browser entirely
 
